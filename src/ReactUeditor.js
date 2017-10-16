b20b1d48a3f44df62421040664b6d6c12394a857
yaokailun <kailunyao@163.com>
2017-10-16 15:16:37 +0800

    add simple insertCode plugins

--- src/ReactUeditor.js
+++ src/ReactUeditor.js
@@ -3,6 +3,8 @@ import React from 'react'
 
 let content = ''  // 存储编辑器的实时数据，用于传递给父组件
 let ueditor, isContentChangedByWillReceiveProps = false, tempfileInput = null
+const simpleInsertCodeIcon = 'data:img/jpg;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAB9klEQVRYR+2Wy23CQBCGZxwUASdKgA7IIdIukhF0QCoI6YAS6CB0EDpIOgjCEbs3nApCB+EEKFI80ToYgR/7IEhIEb4hvPN/8/jHi3DmB8+sDxeA/1GBdosNiTAMhHhxnamTVMDnfAEAo0CI0ckBOs1mbRKGy6LArdZtswSl+VdEDSmlAtk9prPqRW0FfMb66OGjt1o3iiB8zgcAMAiEqKfFo0p5QQSDQMpxUQKFAFvxJ4roQRfA52yCgOFUCAVy8NjEyAWwOaiUVImjauWTCO6KBtAUKwNgOrCfos95DxGepzNh08rcah4cdBFXID5nY0CsBTPRM01/UewdgKu4EmxztiTAoa398jRigGPEdfbTVSOthUkfTdOeDrrdfv20/UytSCeMKhAQ3HvrzY1u4WQs1mIhEk7y7GeCiN1TKc8J8R3Vj+9qWXmZvNW6awOR2C+KqPsm5cQkmFlQ1corAeHVatOJZ8AVIu4jwmgqZO0v4irZnQtcIFzslwBuq7bLPKn0wR6whYjtZ9jxurLvtzmzwUwQrvYryjwBzF2hOojYfgC9YCabpv6bxLWf4yII39J+NuLG+8BvkPJgOpND9TJjrH7t4Yet/VS1vNVmpLO205XsWPvpWuUGoD6/AJ1jtp/zjcg0YKf636kCpxLdj3MBOHsFfgBLLaBN8r49lAAAAABJRU5ErkJggg=='
+const uploadImageIcon = 'data:img/jpg;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAByUlEQVRYR+2XsS5EQRSGv30CSiqUKrQiQalCRccjUKrYTodegidAolPgDXgD3oAKFfk2M8nsjV33rp27JCbZ7N17587/n/P/58xsgwGPxoDx+SfwqzLwkdEPi8BNWL8J7EWsNAMS2Abu+0xEYAk4DoGLbgSceNtnAgYW13VtPx0z8GcJLAC7wDOgxqmMtWRA4KEg3SWwkshYC4G0gu4AMxJHLQQ2gRPgBfBap9dKoFvh/CgDw8AGMA4cAY89lGjPBAS3iUwHUMFngtOr8OiJgKDnIfIUTG1Xq6ADlQlYUkZuBr4atmtbatlRicAZsNwFPIIqRXHP0CdjwENBpkoEykZV9EMsQ983g7b0SDALAYGiH1LwGIAkDoJfoomdP/Hdblg2A3Gei6ZtN33/HVgPu59+sjseA1fpO8XzQFUCnea/AaNBDr3hULY54BpYitt+LgJppcS9P/2WlNK1nYr7eSQzMEHsonFj8iBilXlff1hJWQmouwZsRQqchsrwnn2kZc5cEuwDOwG4KIHPRnJL8ApMAk9JO9eEa2Hbno19Itf/AtM9D2yFzqgc/jYbGtTnrZGLgGurvQSmApZHNbVvO3XnJFCqpwycwCfya6AhY6x1TgAAAABJRU5ErkJggg=='
 
 class ReactUeditor extends React.Component {
 
@@ -69,21 +71,27 @@ class ReactUeditor extends React.Component {
       var btn = new window.UE.ui.Button({
         name: uiName,
         title: '文件上传',
-        cssRules: 'background-position: -380px 0',
+        cssRules: 'background: url(' + uploadImageIcon + ') !important; background-size: 20px 20px !important;',
         onclick: () => {
           tempfileInput.click()
         }
       })
 
-      // 当点到编辑内容上时，按钮要做的状态反射
-      editor.addListener('selectionchange', () => {
-        var state = editor.queryCommandState(uiName)
-        if (state == -1) {
-          btn.setDisabled(true)
-          btn.setChecked(false)
-        } else {
-          btn.setDisabled(false)
-          btn.setChecked(state)
+      return btn
+    })
+  }
+
+  registerSimpleInsertCode() {
+    window.UE.registerUI('simpleInsertCode', (editor, uiName) => {
+      var btn = new window.UE.ui.Button({
+        name: uiName,
+        title: '插入代码',
+        cssRules: 'background: url(' + simpleInsertCodeIcon + ') !important; background-size: 20px 20px !important;',
+        onclick: () => {
+          if (ueditor) {
+            ueditor.focus()
+            ueditor.execCommand('insertcode')
+          }
         }
       })
 
@@ -107,10 +115,13 @@ class ReactUeditor extends React.Component {
   }
 
   initEditor() {
-    const props  = this.props
-
+    const props  = this.props, plugins = props.plugins
     ueditor = window.UE.getEditor('container')
-    this.registerImageUpload()
+
+    if (plugins && plugins instanceof Array && plugins.length > 0) {
+      if (plugins.indexOf('uploadImage') !== -1) this.registerImageUpload()
+      if (plugins.indexOf('insertCode') !== -1) this.registerSimpleInsertCode()
+    }
 
     ueditor.ready(() => {
       ueditor.addListener('contentChange', () => {
@@ -148,6 +159,7 @@ class ReactUeditor extends React.Component {
 ReactUeditor.propTypes = {
   value: PropTypes.string,
   ueditorPath: PropTypes.string.isRequired,
+  plugins: PropTypes.array,
   onChange: PropTypes.func,
   uploadImage: PropTypes.func
 }
