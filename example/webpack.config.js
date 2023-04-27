777683dfd79b56e6f6f2dbf116887f814ec6234c
Jiajun Yan <yanjiajun@ifanr.com>
2023-04-27 10:23:00 +0800

    upgrade deps

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'index.js'),
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, '..'),
+    static: path.resolve(__dirname, '..'),
   },
   plugins: [
     new HtmlWebpackPlugin({
